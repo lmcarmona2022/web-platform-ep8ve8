6cfcc3e27702d692b32f90719edaa77b90c134db
lmcarmona2022 <luismcarmona2020@gmail.com>
2022-07-14 14:12:40 -0300

    1 file modified

--- login.js
+++ login.js
@@ -1,12 +1,13 @@
 function Login(form) {
   username = new Array("admin");
   password = new Array("test");
+ // if (document.getElementsByClassNametElementsByClassName('login__input')==username[0] )  {
   if (form.username.value == username[0] && form.password.value == password[0]) {
   location.href='/';
   return false;
   }
   else {
-  alert("Usuario ó Contraseña incorrecto \n Pruebe nuevamente");
+  alert(document.getElementsByClassName('login__input').value+"Usuario ó Contraseña incorrecto \n Pruebe nuevamente");
   form.username.focus();
   }
   return true;
